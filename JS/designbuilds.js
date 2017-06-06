5020f0b9ba2574c1188ade48a51e2944c9e6fd76
WilliamParlow <william.nichelatti@catolicasc.org.br>
2017-06-06 08:46:51 -0300

    Player-UI design fixed

--- JS/designbuilds.js
+++ JS/designbuilds.js
@@ -6,78 +6,78 @@ var prediosDesign = [];
 
 
 function switchOn(){
-    
-    for (var k=0; k < prediosDesign.length; k++ ){
+
+  for (var k=0; k < prediosDesign.length; k++ ){
     var linhas = Math.floor(prediosDesign[k].sizeY/6);
     var colunas = Math.floor(prediosDesign[k].sizeX/8);
 
     var posicaoLinhas= canvasFooter.height - prediosDesign[k].sizeY+3;
     var posicaoColunas = prediosDesign[k].positionX+3;
     for (var j = 1; j < colunas; j++){
-        
-    for (var i = 1; i < linhas; i++){
-     ctxFooter.fillStyle = "#FFFF00";
-    ctxFooter.fillRect(posicaoColunas,posicaoLinhas,6,3);
-     posicaoLinhas += 6;
+
+      for (var i = 1; i < linhas; i++){
+        ctxFooter.fillStyle = "#FFFF00";
+        ctxFooter.fillRect(posicaoColunas,posicaoLinhas,6,3);
+        posicaoLinhas += 6;
+
+      }
+      posicaoColunas += 9;
+      posicaoLinhas= canvasFooter.height - prediosDesign[k].sizeY+3;
 
     }
-    posicaoColunas += 9;
-    posicaoLinhas= canvasFooter.height - prediosDesign[k].sizeY+3;
-        
-    }
-           
-    }
-    
-    
+
+  }
+
+
 }
 
 function switchOff(){
-    
-    for (var k=0; k < prediosDesign.length; k++ ){
+
+  for (var k=0; k < prediosDesign.length; k++ ){
     var linhas = Math.floor(prediosDesign[k].sizeY/6);
     var colunas = Math.floor(prediosDesign[k].sizeX/8);
 
     var posicaoLinhas= canvasFooter.height - prediosDesign[k].sizeY+3;
     var posicaoColunas = prediosDesign[k].positionX+3;
     for (var j = 1; j < colunas; j++){
-        
-    for (var i = 1; i < linhas; i++){
+
+      for (var i = 1; i < linhas; i++){
         ctxFooter.fillStyle = "#cccccc";
         ctxFooter.fillRect(posicaoColunas,posicaoLinhas,6,3);
         posicaoLinhas += 6;
+      }
+      posicaoColunas += 9;
+      posicaoLinhas= canvasFooter.height - prediosDesign[k].sizeY+3;
+
     }
-    posicaoColunas += 9;
-    posicaoLinhas= canvasFooter.height - prediosDesign[k].sizeY+3;
-        
-    }
-           
-    }
-    
-    
+
+  }
+
+
 }
 
 
 
 
-  
+
 function buildsDesign (){
 
 
-var totalwidth = 0;
-var positionX = 0;
-var sizeX=0;
-var indexDesign =0;
-    
-while (totalwidth < canvasFooter.width){
+  var totalwidth = 0;
+  var positionX = 0;
+  var sizeX=0;
+  var indexDesign =0;
+
+  while (totalwidth < canvasFooter.width){
     var predioDesign = {
-    positionX :0 ,
-    positionY :0,
-    sizeX:0 ,
-    sizeY:0
+      positionX :0 ,
+      positionY :0,
+      sizeX:0 ,
+      sizeY:0
     };
-    
-    
-    
+
+
+
     sizeX = (Math.floor(Math.random() * (80 - 20))+20);
     sizey = (Math.floor(Math.random() * (60 - 20))+20);
     ctxFooter.fillStyle = "#002040";
@@ -87,32 +87,32 @@ while (totalwidth < canvasFooter.width){
     predioDesign.sizeX = sizeX;
     predioDesign.sizeY = sizey;
     prediosDesign[indexDesign] = predioDesign;
-    
+
     var linhas = Math.floor(sizey/6);
     var colunas = Math.floor(sizeX/8);
 
     var posicaoLinhas= canvasFooter.height - sizey+3;
     var posicaoColunas = positionX+3;
     for (var j = 1; j < colunas; j++){
-        
-    for (var i = 1; i < linhas; i++){
+
+      for (var i = 1; i < linhas; i++){
         ctxFooter.fillStyle = "#cccccc";
         ctxFooter.fillRect(posicaoColunas,posicaoLinhas,6,3);
         posicaoLinhas += 6;
+      }
+      posicaoColunas += 9;
+      posicaoLinhas= canvasFooter.height - sizey+3;
+
     }
-    posicaoColunas += 9;
-    posicaoLinhas= canvasFooter.height - sizey+3;
-        
-    }
-    
+
     totalwidth += sizeX;
-    positionX+= sizeX + 3; 
+    positionX+= sizeX + 3;
     indexDesign++;
+  }
 }
-}
-    
-  
-    
-    
+
+
+
+
 
 buildsDesign();
